a0910aedcace34e8868a588e8f6bf3ec5f3ee476
ZadoR205 <zaidval@gmail.com>
2025-06-03 13:11:04 -0500

    feat: support service
    Docker-compose container configuration

---
+++ Support-Service/src/main/java/com/fromzero/backend/FromZeroBackendApplication.java
@@ -0,0 +1,24 @@
+package com.fromzero.backend;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+
+public class FromZeroBackendApplication implements CommandLineRunner {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromZeroBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
