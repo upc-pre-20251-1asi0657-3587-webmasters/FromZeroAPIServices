2dae93df816a2ab20e48551734fb2a36326c80ce
Sr-Anonymus-maker <carlosleonrioja@gmail.com>
2025-06-06 23:30:59 -0500

    feat: get user by id endpoint added

--- Auth-Service/src/main/java/com/authservice/iam/interfaces/rest/UsersController.java
+++ Auth-Service/src/main/java/com/authservice/iam/interfaces/rest/UsersController.java
@@ -1,5 +1,6 @@
 package com.authservice.iam.interfaces.rest;
 
+import com.authservice.iam.domain.model.queries.GetUserByIdQuery;
 import com.authservice.iam.domain.services.UserQueryService;
 import com.authservice.iam.interfaces.rest.resources.UserResource;
 import com.authservice.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
@@ -10,10 +11,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1/users", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -35,5 +38,20 @@ public class UsersController {
         var users = userQueryService.handle();
         var userResources = users.stream().map(UserResourceFromEntityAssembler::toResourceFromEntity).toList();
         return ResponseEntity.ok(userResources);
+    }
+
+    @GetMapping(value = "/{userId}")
+    @Operation(summary = "Get user by id", description = "Get the user with the given id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User retrieved successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found."),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.")
+    })
+    public ResponseEntity<UserResource> getUserById(@PathVariable UUID userId) {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var user = userQueryService.handle(getUserByIdQuery);
+        if (user.isEmpty()) return ResponseEntity.notFound().build();
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return ResponseEntity.ok(userResource);
     }
 }
