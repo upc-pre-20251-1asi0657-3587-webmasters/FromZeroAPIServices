4fdd5872359781b3dffac93d5026e76a5652a73b
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-06-11 18:13:30 -0500

    feat: adding docker configuration for notifications service

--- Notification-Service/src/main/java/com/fromzero/notificationservice/NotificationServiceApplication.java
+++ Notification-Service/src/main/java/com/fromzero/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,12 @@ package com.fromzero.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
