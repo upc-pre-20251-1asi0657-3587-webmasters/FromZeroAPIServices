3457fedf6e94e95c188d60923a224163cf7d2951
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-06-28 01:03:31 -0500

    chore: init docker configuration for auth, user, project and deliverables

--- Deliverables-Service/src/main/java/com/fromzero/deliverableservice/deliverables/domain/events/DeliverableCreatedEvent.java
+++ Deliverables-Service/src/main/java/com/fromzero/deliverableservice/deliverables/domain/events/DeliverableCreatedEvent.java
@@ -5,13 +5,33 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-
 public class DeliverableCreatedEvent {
     private Long projectId;
     private String deliverableName;
     private LocalDateTime deadline;
 
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getDeliverableName() {
+        return deliverableName;
+    }
+
+    public void setDeliverableName(String deliverableName) {
+        this.deliverableName = deliverableName;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
 }
 
