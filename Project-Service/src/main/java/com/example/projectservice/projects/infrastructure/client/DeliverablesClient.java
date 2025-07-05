754e95eb839138cdd29c5f92c6c8deee39239259
Jair Velasquez <jairvelazpizar@gmail.com>
2025-07-05 07:01:14 -0500

    feat: adding release configuration for k8s and docker-compose for deploy

--- Project-Service/src/main/java/com/example/projectservice/projects/infrastructure/client/DeliverablesClient.java
+++ Project-Service/src/main/java/com/example/projectservice/projects/infrastructure/client/DeliverablesClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "deliverablesservice") // nombre registrado en Eureka
+@FeignClient(name = "Deliverables-Service") // nombre registrado en Eureka
 public interface DeliverablesClient {
 
     @PostMapping("/api/v1/default-deliverables")
